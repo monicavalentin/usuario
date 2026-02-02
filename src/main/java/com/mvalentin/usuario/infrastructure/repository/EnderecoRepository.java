caf3bf690796cec00ecd652fcbb82b4a007f2c9d
Monicat263 <mda263@bol.com.br>
2026-02-02 16:33:51 -0300

    Criação método PUT atualiza endereco

---
+++ src/main/java/com/mvalentin/usuario/infrastructure/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.mvalentin.usuario.infrastructure.repository;
+
+import com.mvalentin.usuario.infrastructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
